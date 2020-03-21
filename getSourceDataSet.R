b76b0267cc84d52b715be01f45765f2d00959cdc
jravier <60151194+jravier@users.noreply.github.com>
2020-03-21 23:29:47 +0100

    More comments in getSourceDataSet.R

--- getSourceDataSet.R
+++ getSourceDataSet.R
@@ -1,5 +1,19 @@
 urlDataSource="https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip"
 
+######  grabing the dataset  ######
+# this file is called [using source('getSourceDataSet.R')] 
+#        by all the plotting scripts to read the data
+# The 1st time it's called, it will download the zip, 
+#       extract the txt file,
+#       load the data in a data.frame named 'epcDataset', 
+#       filter it on the 2 asked dates,
+#       and replace the 2 Date & Time columns by a single POSIXct column named 'instant'
+# the next times it's called, it just checks that the 'epcDataset' data.frame exists
+#       if yes, comes back here without doing anything else and continues
+#       if no, proceeds again as above before !
+# The code checking the existence of the data set is a the bottom of the file 
+#       after the downloading function
+
 library(data.table)
 library(dplyr)
 #library(lubridate)
@@ -26,6 +40,7 @@ datadwnld<-function(theurl, datadir="."){
     }
 }
 
+# check the existence of the data set. If not, proceed !
 if (!exists("epcDataset"))
 epcDataset=fread(datadwnld(urlDataSource, "data"), na.strings = "?", 
                  colClasses = c("factor", "factor", "numeric", 
@@ -35,5 +50,7 @@ epcDataset=fread(datadwnld(urlDataSource, "data"), na.strings = "?",
         mutate(instant=as.POSIXct(strptime(paste(Date, Time), 
                                            "%d/%m/%Y %H:%M:%S"))) %>% 
         select(10, 3:9)
+
+
 
 #as.POSIXct(dmy(Date) +hms(Time)) # removed the need for lubridate
